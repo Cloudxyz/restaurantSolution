1ef7e216e801d5715a65aea55f2a1fd768247f97
LAPTOP-7QL3U6CS\alan <cloudzeroxyz@gmail.com>
2022-03-12 13:49:45 -0600

    install admintle

--- resources/views/layouts/menu.blade.php
+++
@@ -1,8 +0,0 @@
-<li class="nav-item">
-    <a href="{{ route('restaurants.index') }}"
-       class="nav-link {{ Request::is('restaurants*') ? 'active' : '' }}">
-        <p>Restaurants</p>
-    </a>
-</li>
-
-
